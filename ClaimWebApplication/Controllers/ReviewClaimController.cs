1d34e0e3b530642bd32ff032dbd1c99f61665d27
Lindokuhle Zwane <ST10381088@rcconnect.edu.za>
2024-11-22 22:44:47 +0200

    Created a ReviewClaimController that is responsible to view or output all the claims made by lecturers and be reviews by progam co-ordinator

--- ClaimWebApplication/Controllers/ReviewClaimController.cs
+++ ClaimWebApplication/Controllers/ReviewClaimController.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using ClaimWebApplication.Data;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace ClaimWebApplication.Controllers
+{
+    public class ReviewClaimController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        // a constructor to include our database 
+        public ReviewClaimController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
